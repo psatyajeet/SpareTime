378d07b8ed4a7db3b1cc323471d286d8bcad2c89
Satyajeet Pal <psatyajeet@gmail.com>
2013-03-20 21:54:47 -0400

    Change models

--- mysite/socialcalendar/models.py
+++ mysite/socialcalendar/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+class UserProfile(models.Model):
+    home_address = models.TextField()
+    user = models.ForeignKey(User, unique=True)
+    events = models.ManyToManyField(Event)
+    
+    def __unicode__(self):
+        return self.user.name
