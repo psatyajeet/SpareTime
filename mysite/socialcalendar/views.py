e5ea5e28f593be720a5cd333296c29369e15ea10
Sammy Jerome <sam@intuary.com>
2013-03-29 02:32:53 -0400

    added integration with google calendar

--- mysite/socialcalendar/views.py
+++ mysite/socialcalendar/views.py
@@ -1,3 +1,12 @@
+import httplib2
+import sys
+
+from apiclient.discovery import build
+from oauth2client.file import Storage
+from oauth2client.client import AccessTokenRefreshError
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.tools import run
+
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.csrf import ensure_csrf_cookie
@@ -267,4 +276,75 @@ def changeStart(request):
 
 @csrf_protect
 def gcal(request):
+    client_id = '984176055001-8kq2incml2fo37fb1l9e8spu0j3uk906.apps.googleusercontent.com'
+    client_secret = 'UInVj9loCowEztBqN8EcCwZV'
+
+    # The scope URL for read/write access to a user's calendar data
+    scope = 'https://www.googleapis.com/auth/calendar'
+
+    # Create a flow object. This object holds the client_id, client_secret, and
+    # scope. It assists with OAuth 2.0 steps to get user authorization and
+    # credentials.
+    flow = OAuth2WebServerFlow(client_id, client_secret, scope)
+
+    # Create a Storage object. This object holds the credentials that your
+    # application needs to authorize access to the user's data. The name of the
+    # credentials file is provided. If the file does not exist, it is
+    # created. This object can only hold credentials for a single user, so
+    # as-written, this script can only handle a single user.
+    storage = Storage('credentials.dat')
+
+    # The get() function returns the credentials for the Storage object. If no
+    # credentials were found, None is returned.
+    credentials = storage.get()
+
+    # If no credentials are found or the credentials are invalid due to
+    # expiration, new credentials need to be obtained from the authorization
+    # server. The oauth2client.tools.run() function attempts to open an
+    # authorization server page in your default web browser. The server
+    # asks the user to grant your application access to the user's data.
+    # If the user grants access, the run() function returns new credentials.
+    # The new credentials are also stored in the supplied Storage object,
+    # which updates the credentials.dat file.
+    if credentials is None or credentials.invalid:
+        credentials = run(flow, storage)
+
+    # Create an httplib2.Http object to handle our HTTP requests, and authorize it
+    # using the credentials.authorize() function.
+    http = httplib2.Http()
+    http = credentials.authorize(http)
+
+    # The apiclient.discovery.build() function returns an instance of an API service
+    # object can be used to make API calls. The object is constructed with
+    # methods specific to the calendar API. The arguments provided are:
+    #   name of the API ('calendar')
+    #   version of the API you are using ('v3')
+    #   authorized httplib2.Http() object that can be used for API calls
+    service = build('calendar', 'v3', http=http)
+    page_token = None
+
+    while True:
+        events = service.events().list(calendarId='primary').execute()
+        if events['items']:
+            for event in events['items']:
+                if not event.has_key('summary'):
+                    event['summary'] = ''
+                if not event.has_key('description'):
+                    event['description'] = ''
+                if not event.has_key('location'):
+                    event['location'] = ''
+                if not event.has_key('start'):
+                    event['start']['dateTime'] = ''
+                if not event.has_key('end'):
+                    event['end']['dateTime'] = ''
+                e = Event(title=event['summary'],
+                          description=event['description'],
+                          location=event['location'],
+                          start=event['start']['dateTime'].replace("T", " "),
+                          end=event['end']['dateTime'].replace("T", " "),
+                          )
+                e.save()
+        page_token = events.get('nextPageToken')
+        if not page_token:
+            break
     return HttpResponse()
