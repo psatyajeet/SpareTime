cf00c946354597a717abdb0d55691b81b2b38a5f
Sammy Jerome <sam@intuary.com>
2013-04-22 21:04:50 -0400

    added dragrepeat

--- mysite/socialcalendar/views.py
+++ mysite/socialcalendar/views.py
@@ -331,7 +331,6 @@ def populateEvents(request):
     
     events = getAllEvents(usr, first, last, ['PU', 'PR', 'FL'])
     d = getArrayofWeeklyEvents(events, usr)
-
     return HttpResponse(simplejson.dumps(d))
 
 def getArrayofWeeklyEvents(events, usr):
@@ -368,10 +367,12 @@ def getArrayofWeeklyEvents(events, usr):
             endhour = 24    
         eID = e.id;
         if e.repeat:
-            eID = None;
-        creator0 = None;
-        if(len(e.creators.all()) > 0):
-            creator0=e.creators.all()[0].name 
+            eID = e.repeatID;
+            creator0 = e.creators[0].name
+        elif (len(e.creators.all()) > 0):
+            creator0 = e.creators.all()[0].name 
+        else:
+            creator0 = None
         d.append({
             'title': e.title,
             'start': e.start.hour+e.start.minute/60.0,
@@ -429,7 +430,7 @@ def populateMonthEvents(request):
 def getEventData(request):
 
     if request.method == "POST":
-        events = Event.objects.filter(id=request.POST['id'])
+        events = Event.objects.filter(id=findIdOfEvent(request.POST['id']))
 
         if (len(events) != 1):
             return HttpResponseNotFound()
@@ -701,15 +702,19 @@ def getWeeklyRecurringEvents(usr, first, last):
         for time in times:
             if len(event.exceptions.filter(exceptionTime=time)) > 0:
                 continue
-            e = Event(
+
+            e = tempEvent(
             title=event.title,
             description=event.description,
             location=event.location,
             start=datetime(time.year ,time.month ,time.day , event.start.hour, event.start.minute).replace(tzinfo=tz.gettz('UTC')),
             end=datetime(time.year, time.month, time.day, event.end.hour, event.end.minute).replace(tzinfo=tz.gettz('UTC')),
             repeat = True,
+            repeatID = str(event.id) + '_' + str(time),
+            eid = event.id,
+            creators = event.creators.all()
             )
-            e.id = event.id
+
             totalForWeek.append(e)
 
     return totalForWeek
@@ -727,3 +732,19 @@ def addCreators(event, creatorsArray):
     for creator in creatorsArray:
         creator.creators.add(event)
 
+def findIdOfEvent(idToSearch):
+    if idToSearch.rfind("_") == -1:
+        return idToSearch
+    return idToSearch[0, idToSearch.rfind("_")]
+
+class tempEvent:
+    def __init__(self, title, description, location, start,end, repeat, repeatID, eid, creators):
+        self.title = title
+        self.description = description
+        self.location = location,
+        self.start = start
+        self.end = end
+        self.repeat = repeat
+        self.repeatID = repeatID
+        self.id = eid
+        self.creators = creators
